578b7b9f8836f3a979a54d7ec2915dd5c9361d2b
Eduinfer03 <119307257+Eduinfer03@users.noreply.github.com>
2023-09-07 09:02:02 -0500

    Add files via upload

---
+++ cuatro.js
@@ -0,0 +1,15 @@
+let num = 550;
+let primo = [];
+for(let i = 250; i<= num; i++){
+    esPrimo = true;
+    if (i === 0)
+    console.log('no es primo')
+    if (i%2===0)
+        for (let j = 250; j<=num; j++){
+        primo.push
+    }
+    else
+    primo(true)
+    console.log(primo)
+
+}
