8824612f65b336c80fa4e9a5cea2493429e596f3
Eduinfer03 <119307257+Eduinfer03@users.noreply.github.com>
2023-09-07 08:39:05 -0500

    Segundo

---
+++ dos.js
@@ -0,0 +1,9 @@
+let empleado = "Andres";
+let sueldoMes = 100;
+let bonificacionMes = 5;
+
+let sueldoAnual = sueldoMes * 12;
+let bonificacionAnual = bonificacionMes * 12;
+
+let sueldoNeto = sueldoAnual + bonificacionAnual;
+console.log(`el sueldo anual de ${empleado} es de ${sueldoNeto}`)
